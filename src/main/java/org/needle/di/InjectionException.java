6f61574cdb22e72fc101699bcee7bed6102a7fd2
Fabien Le Houëdec <fabien.lehouedec@gmail.com>
2017-12-22 13:43:26 +0100

    Translated exception error messages

--- src/main/java/org/needle/di/InjectionException.java
+++ src/main/java/org/needle/di/InjectionException.java
@@ -4,11 +4,11 @@ public class InjectionException extends Exception {
 
 	private static final long serialVersionUID = 4701495830786103003L;
 	
-	public final static String INSTANCIATION_FAILED = "Impossible d'instancier le service %s. Avez-vous oublier le constructeur vide ?";
-	public final static String CYCLIC_DEPENDENCIES  = "La classe %s apparaît déjà dans le graphe de dépendances, il y a donc une référence circulaire entre les services.";
-	public final static String INJECTION_FAILED     = "Impossible d'injecter une instance dans le champ %s.";
-	public final static String NESTED_EXCEPTION     = "Impossible de créer la dépendance à injecter dans le champ %s. Exception imbriquée : %s";
-	public final static String UNAVAILABLE_PARAM    = "Le paramètre %s n'a pas pu être résolu avec la configuration de l'injecteur. Le paramètre a-t-il été déclaré ?";
+	public final static String INSTANCIATION_FAILED = "Unable to instanciated %s service. Did you declare an empty constructor ?";
+	public final static String CYCLIC_DEPENDENCIES  = "Class %s has already been proceeded. Seems there is a cyclic dependency.";
+	public final static String INJECTION_FAILED     = "Unable to inject a matching instance in the field %s.";
+	public final static String NESTED_EXCEPTION     = "Unable to create the dependency instance to inject in the field %s. Nested exception : %s";
+	public final static String UNAVAILABLE_PARAM    = "The parameter %s could not be resolved with the builder configuration. Had the parameter been declared ?";
 
 	public InjectionException(String message, Throwable cause) {
 		super(message, cause);
